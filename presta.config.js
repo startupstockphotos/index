9753edcf7ab48bd7774115180f2a0f54943651cc
estrattonbailey <ericstrattonbailey@gmail.com>
2020-11-01 15:29:00 -0600

    add old GA and FB pixel

--- presta.config.js
+++ presta.config.js
@@ -60,14 +60,20 @@ export function createDocument (ctx) {
         {
           children: `"function"!=typeof Object.assign&&Object.defineProperty(Object,"assign",{value:function(e,t){"use strict";if(null==e)throw new TypeError("Cannot convert undefined or null to object");for(var n=Object(e),r=1;r<arguments.length;r++){var o=arguments[r];if(null!=o)for(var c in o)Object.prototype.hasOwnProperty.call(o,c)&&(n[c]=o[c])}return n},writable:!0,configurable:!0});`
         },
-        `<script async src="https://www.googletagmanager.com/gtag/js?id=G-KEYFNM7409"></script>
+
+        // Google Analytics
+        `
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-50384201-1"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-KEYFNM7409');
-        </script>`,
+          gtag('config', 'UA-50384201-1');
+        </script>
+        `,
+
+        // banner ad
         `<script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
         <script>
           window.googletag = window.googletag || {cmd: []};
@@ -77,7 +83,21 @@ export function createDocument (ctx) {
             googletag.enableServices();
           });
         </script>
-        `
+        `,
+
+        // FB Pixel
+        `<script>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+        document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1889140964685634'); // Insert your pixel ID here.
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=1889140964685634&ev=PageView&noscript=1"
+        /></noscript>`
       ]
     }
   })
